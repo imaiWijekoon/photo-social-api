6d68828cbc97ae428ec8053cb0129d9a1ab915f7
Devinda Vidanapathirana <devinda.vidanapathirana@gmail.com>
2025-05-02 22:20:45 +0530

    feat: update

--- src/main/java/com/example/photo/controller/PostController.java
+++ src/main/java/com/example/photo/controller/PostController.java
@@ -5,7 +5,6 @@ import com.example.photo.service.PostService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-
 import java.util.List;
 
 @RestController
